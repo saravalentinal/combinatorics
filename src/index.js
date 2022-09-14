7eb89c3e4441bcf80225fa73d4429e42fc658579
saravalentinal <latynsara41@gmail.com>
2022-09-14 18:19:32 +0200

    bootstrap added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom'
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 import App from './App';
 
